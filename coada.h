1054227f5c6e1f8beb36ecc46cefcb818293d563
TraianV <61265978+TraianV@users.noreply.github.com>
2020-05-11 00:39:37 +0300

    Update coada.h

--- coada.h
+++ coada.h
@@ -1,26 +1,16 @@
 #ifndef UNTITLED_COADA_H
 #define UNTITLED_COADA_H
 #include "vector.h"
-#include "complex.h"
+#include "complex"
 #include<iostream>
 
-class coada
+class coada : public vector
 {
-    vector vec;
 public:
     friend class vector;
     friend class complex;
-    coada(vector vec);
-    coada (const coada &c);
-    ~coada();
-    coada &push(complex x);
+    coada &push(const complex &x);
     coada &pop();
-    vector get_v()
-    {
-        return this->vec;
-    }
-    friend std::istream & operator >>(std::istream &, coada & );
-    friend std::ostream & operator <<(std::ostream &, coada & );
 };
 
 

67a7e0c71cc4e2dc8aa6062c9dc90810523c2bd2
TraianV <61265978+TraianV@users.noreply.github.com>
2020-05-11 00:42:50 +0300

    Update vector.h

--- vector.h
+++ vector.h
@@ -9,18 +9,35 @@ class vector
     int dim;
 public:
     friend class complex;
+    friend class stiva;
+    friend class coada;
     vector(complex *vec,int n);
     vector();
     vector(vector const &);
     ~vector();
-    int get_n(){return this->dim;}
-    void set_n(int n){this->dim=n;}
-    complex* get_v(){return this->v;}
-    void set_v(int n){this->v=new complex[n];}
-    void set_v(){delete []this->v;}
+    int get_n()
+    {
+        return this->dim;
+    }
+    void set_n(int n)
+    {
+        this->dim=n;
+    }
+    complex* get_v()
+    {
+        return this->v;
+    }
+    void set_v(int n)
+    {
+        delete[] this->v;
+        this->v=new complex[n];
+    }
     complex operator[](const int ) const;
     vector & operator=(const vector &vec);
-    void set_v(int i,complex x){this->v[i]=x;}
+    void set_v(int i,complex x)
+    {
+        this->v[i]=x;
+    }
     friend std::istream & operator >>(std::istream &in, vector &);
     friend std::ostream & operator <<(std::ostream &out,const vector &);
 };

6dd89bb01bd8e1614d87245171b40e74ce0cc85b
TraianV <61265978+TraianV@users.noreply.github.com>
2020-05-11 12:44:51 +0300

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,5 +1,5 @@
 #include "vector.h"
-#include "complex"
+#include "complex.h"
 #include "stiva.h"
 #include "coada.h"
 #include<fstream>
@@ -54,7 +54,7 @@ std::istream &operator >> (std::istream & in, vector & vec)
     int i;
     if(vec.dim>0)
         delete []vec.v;
-    std::cout<<"Dimensiunea vectorului:";
+    std::cout<<"Dimensiunea vectorului: ";
     in>>vec.dim;
     vec.v=new complex[vec.dim];
     for(i=0; i<vec.dim; i++)

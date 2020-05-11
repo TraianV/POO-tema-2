9179701269f215838aea04f873a27822674278fa
TraianV <61265978+TraianV@users.noreply.github.com>
2020-05-11 12:40:07 +0300

    Update coada.cpp

--- coada.cpp
+++ coada.cpp
@@ -9,9 +9,11 @@ coada & coada::push(const complex &x)
     for(i=0;i<this->get_n();i++)
         vec.set_v(i,this->get_v()[i]);
     vec.set_v(this->get_n(),x);
-    for(i=0;i<this->dim;i++)
+    delete [] this->v;
+    this->v=new complex[vec.dim];
+    for(i=0;i<vec.dim;i++)
         this->v[i]=vec.v[i];
-    this->dim++;
+    this->dim=vec.dim;
     return *this;
 }
 coada & coada::pop()
@@ -23,9 +25,12 @@ coada & coada::pop()
         vec.set_n(this->dim-1);
         vec.set_v(this->dim-1);
         for(i=1; i<this->dim; i++)
-            vec.v[i]=this->v[i];
+            vec.v[i-1]=this->v[i];
+        delete [] this->v;
+        this->v=new complex[vec.dim];
         for(i=0;i<vec.dim;i++)
             this->v[i]=vec.v[i];
+        this->dim=vec.dim;
         return *this;
     }
     else
